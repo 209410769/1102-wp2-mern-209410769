cf114f86ef91789ab308c122d5921ec8e084f919
209410769 <209410769@gms.tku.edu.tw>
2022-03-24 21:12:50 +0800

    first commit

---
+++ ClassDemo/W05/Step2_Add_landing_69/clinet_69/client_69/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'normalize.css';
+import App_69 from './App_69';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App_69 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ client_69/src/App_69.js
@@ -0,0 +1,24 @@
+import Landing_69 from "./pages/Landing_69";
+import styled from 'styled-components'
+
+// const Button = styled.button`
+// background:red;
+// color:white;
+// font-size:1rem;
+// `
+// const ButtonSecond = styled.button`
+// background:blue;
+// color:white;
+// font-size:1rem;`
+
+function App_69() {
+  return (
+    <div>
+      {/* <Button>Click me</Button>
+      <ButtonSecond>Click me</ButtonSecond> */}
+      <Landing_69 />
+    </div>
+  );
+}
+
+export default App_69;
