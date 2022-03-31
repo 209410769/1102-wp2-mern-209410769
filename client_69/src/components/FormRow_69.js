c624e31c5e56a2c849c12bbdb93d11d37e4de45e
209410769 <209410769@gms.tku.edu.tw>
2022-03-31 21:34:58 +0800

    Test e.target on Form fields (name,email,password)

---
+++ client_69/src/components/FormRow_69.js
@@ -0,0 +1,16 @@
+const FormRow_69 = ({ type, name, vlaue, handleChange, labelText }) => {
+    return (
+        <div>
+            <label htmlFor={name} className="form-label">
+                {labelText || name}
+            </label>
+            <input type={type}
+                value={vlaue}
+                name={name}
+                className='form-input'
+                onChange={handleChange} />
+        </div>
+    )
+}
+
+export default FormRow_69
--- client_69/src/pages/Register_69.js
+++ client_69/src/pages/Register_69.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import Wrapper from "../assets/wrappers/Register_69";
+import { Logo_69, FormRow_69 } from "../components";
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+    showAlert: false
+}
 
 const Register_69 = () => {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log('e.target', e.target);
+        setValues({ ...values, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('e.target', e.target);
+    }
     return (
-        <div>Register_69</div>
+        <Wrapper>
+            <form className="form" onSubmit={onSubmit}>
+                <Logo_69 />
+                <h3>Register</h3>
+                {/* name input */}
+                <FormRow_69
+                    type='text'
+                    name='name'
+                    value={values.name}
+                    handleChange={handleChange}
+                />
+                {/* email input */}
+                <FormRow_69
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    handleChange={handleChange}
+                />
+                {/* password input */}
+                <FormRow_69
+                    type='password'
+                    name='password'
+                    value={values.password}
+                    handleChange={handleChange}
+                />
+                {/* <div className="form-row">
+                    <label htmlFor="name" className="form-label">
+                        name
+                    </label>
+                    <input type="text"
+                        value={values.name}
+                        name='name'
+                        className='form-input'
+                        onChange={handleChange} />
+                </div> */}
+
+                <button className="btn btn-block" type="submit">Submit</button>
+            </form>
+        </Wrapper>
     )
 }
 
